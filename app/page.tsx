4854ec2114b1c15e6b09a7f40e4fa49c587334aa
studentegshawming <alrashidicontractingcompany@gmail.com>
2026-06-24 17:11:53 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
 export default function Home() {
-  return <h1>الكبسولة التعليمية</h1>;
+  return (
+    <main
+      style={{
+        textAlign: "center",
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>🎓 الكبسولة التعليمية</h1>
+
+      <p>مرحبًا بك في موقع الكبسولة التعليمية</p>
+
+      <button
+        onClick={() =>
+          window.open(
+            "https://wa.me/201271825595",
+            "_blank"
+          )
+        }
+      >
+        تواصل عبر واتساب
+      </button>
+    </main>
+  );
 }
